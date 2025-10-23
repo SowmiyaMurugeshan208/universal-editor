513b8960c947cc0863f7326730e5acbcb378118a
Sowmiya Murugeshan <sowmiya.murugeshan@bounteous.com>
2025-10-23 11:51:25 +0530

    Commiting changes related to block to main for learning.

--- blocks/quote/quote.js
+++ blocks/quote/quote.js
@@ -1,6 +1,14 @@
+import { readBlockConfig } from '../../scripts/aem.js';
+
+
 export default function decorate(block) {
   const [quoteWrapper] = block.children;
-  const blockquote = document.createElement('blockquote');
-  blockquote.textContent = quoteWrapper.textContent.trim();
-  quoteWrapper.replaceChildren(blockquote);
+//   const blockquote = document.createElement('blockquote');
+//   blockquote.textContent = quoteWrapper.textContent.trim();
+//   quoteWrapper.replaceChildren(blockquote);
+const cfg = readBlockConfig(block) || {};
+cfg.classes = cfg.classes || 'bg-red';
+  if (cfg.classes) {
+    quoteWrapper.classList.add(cfg.classes);
+  }
 }
